5fb38f7d2b9ae0f02de9c70ca6ad609b19894ae4
adaojreess <adaojreess@gmail.com>
2020-06-18 22:07:21 -0300

    sending messages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
+var port = 3001;
+
+var messages = [];
+
+
 io.on('connection', socket => {
     console.log('Socket: ' + socket.id);
+
+    io.emit('messages', messages);
+
+    socket.on('send', message => {
+        message.time = Date();
+        messages.push(message);
+        io.emit('messages', messages);
+    });
+
+    socket.on('delete', data => {
+        if (data.message.sender === data.sender) {
+            messages = messages.filter(message => message !== data.message);
+        }
+    });
 });
 
-server.listen(3001);
+server.listen(port, () => console.log('Server is running: ' + port));
