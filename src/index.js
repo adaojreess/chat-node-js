a5479344871d5f58ec8a3c7c70d72e526b270256
adaojreess <adaojreess@gmail.com>
2020-06-21 21:38:46 -0300

    page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-var port = process.env.PORT || 3001;
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
 var messages = [];
-
 
 io.on('connection', socket => {
     console.log('Socket: ' + socket.id);
@@ -26,8 +33,5 @@ io.on('connection', socket => {
     });
 });
 
-app.use('/', (req, res) => {
-    res.send('api initialized');
-});
 
-server.listen(port, () => console.log('Server is running: ' + port));
+server.listen(process.env.PORT || 3000, () => console.log('Server is running'));
